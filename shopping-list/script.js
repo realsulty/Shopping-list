8f598a2bd6e0177e289c7b3619396e705a81c24b
realsulty <Sultanobm1@gmail.com>
2023-12-08 23:28:42 +0000

    added new js scripts

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -1,14 +1,35 @@
-// Quick and messy way 
-function createListItem(item) {
-    const li = document.createElement('li');
+// // Quick and messy way 
+// function createListItem(item) {
+//     const li = document.createElement('li');
     
 
-    li.innerHTML = `${item}
-    <button class="remove-item btn-link text-red">
-      <i class="fa-solid fa-xmark"></i>
-    </button>    
-    `; 
-    document.querySelector('.items').appendChild(li)
-    item = document.getElementById('item-input');
-}
+//     li.innerHTML = `${item}
+//     <button class="remove-item btn-link text-red">
+//       <i class="fa-solid fa-xmark"></i>
+//     </button>    
+//     `; 
+//     document.querySelector('.items').appendChild(li)
+//     item = document.getElementById('item-input');
+// }
 // createListItem('banan');
+
+
+
+// now This is more efficent way of writing code 
+
+function newListItem(item){
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(item)); 
+    
+    const button = document.createElement('button');
+    button.className = 'remove-item btn-link text-red';
+
+    const icon = document.createElement('i');
+    icon.className = 'fa-solid fa-xmark';
+    button.appendChild(icon);
+    li.appendChild(button);
+    document.querySelector('.items').appendChild(li);
+
+};
+
+newListItem('bxanadda');
